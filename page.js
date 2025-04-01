f3dd81c9d2389cd5a6c36d6c25473082ac2e38ac
Faizaan <113382577+bhematfa@users.noreply.github.com>
2025-04-01 11:43:03 -0400

    Delete page.js

--- page.js
+++
@@ -1,8 +0,0 @@
-export default function Csc309E11Page() {
-  return (
-    <div id="auto-tester">
-      Hi. I am bhematfa, and I have learned how to develop a full-stack web app
-      and deploy it!
-    </div>
-  );
-}
